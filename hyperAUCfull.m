191fe5c77317f03085e276a74264b2f3c20e57d0
nechernavsky <66107349+nechernavsky@users.noreply.github.com>
2026-01-15 16:43:20 -0500

    Add files via upload

---
+++ hyperAUCfull.m
@@ -0,0 +1,96 @@
+%% Generate CDFs and AUCs for THG hyperintensities
+% Nicole Chernavsky 2025
+%% Threshold 
+% The script will read each file in a folder.
+% Create a binary mask of all the clusters by global threshold that is given by user.
+% Compute the shape measurment (area) of each cluster in a frame.
+% The areas of each cluster is stored as a cumulative distribution function (CDF) for each file.
+%% Steps
+% 1) Each RGB frame is converted to gray scale --> it is unit8
+% 2) Generate a binary mask to segment clusters. 
+% 3) Compute the Area of each cluster and generate a CDF.
+% 4) Repeat for all frames.
+% 5) Pad CDFs to be same x limits for each mouse/folder.
+% 6) Calculate the integral of the full 'padded' CDFs.
+%% How to Use
+% Input
+%   -- Give the percent threshold of interest
+% Output 
+%   -- CDF of cluster sizes and area-under-curve of CDFs for each file.
+%% Acknowledgement
+% Modified from: Sajid Ali, Sungkyunkwan University, Suwon, South Korea
+% Date: |May 31, 2022|
+% Contact: sajidali@skku.edu
+%% 
+function [vq1, auc] = hyperAUCfull(percent)
+
+% select folder
+pname = [uigetdir '/'];
+files = dir([pname '*.tif']);
+num_frames = length(files);
+
+% initialize variable to merge all timepoints into one 3-D array
+video = zeros(512,512,num_frames);
+for j = 1:num_frames
+    % read tiff files and add into 3-D array
+    video(:,:,j) = tiffreadVolume([pname files(j).name]);
+end
+
+%initialize other variables
+measurements = cell(1,num_frames);
+auc = zeros(1,num_frames);
+x_length = zeros(1,num_frames);
+
+%calculate AUC at % threshold
+numVals = floor(512*512*percent/100);
+for i = 1:num_frames
+    % Convert to the gray image
+    grayImage = video(:,:,i);
+
+    % Creates a binary image from 2-D grayscale image by replacing all values above 
+    % threshold with 1 and setting all other values to 0
+    grayVec = reshape(grayImage,[512*512,1]);
+    roi = sort(grayVec,'descend'); 
+    T = roi(numVals);
+    mask = imbinarize(grayImage, T);
+    
+    %acquires areas of each pixel group
+    measurements{i} = regionprops('table', mask, grayImage, 'Area');
+    
+    %cdf of pixel areas
+    h1 = cdfplot(measurements{i}.Area);
+    h1newX{i} = h1.XData(3:2:end-1);
+    h1newY{i} = h1.YData(3:2:end-1);
+    %figure(1);hold on;plot(h1newX,h1newY); %for checking
+    
+    %store sizes of each cdf's x-axis
+    x_length(i) = h1newX{i}(end);
+end  
+    
+%find longest cdf in x
+[maxX,ind] = max(x_length);
+%set this to be the length for all cdfs
+x = 1:1:maxX;
+
+%auc of cdf
+vq1 = cell(num_frames);
+for i = 1:num_frames
+    %pad shorter cdfs to match length of longest one
+    if length(h1newX{i}) < maxX
+        filler = maxX - h1newX{i}(end);
+        h1newX{i} = [h1newX{i} x(end-filler+1:end)];
+        h1newY{i} = [h1newY{i} ones(1,filler)];
+    end
+    %interpolate CDF
+    vq1{i} = interp1(h1newX{i},h1newY{i},x);
+    %calculate integral of CDF
+    auc(i) = trapz(x,vq1{i});
+end
+
+figure; plot(x,vq1{1});
+for i = 2:num_frames
+hold on;plot(x,vq1{i});
+end
+hold off;
+
+end
